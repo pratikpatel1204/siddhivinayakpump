45e8121a1d542a4ebb802f549ed507ac88c7bc62
pratikpatel1204 <www.pratikpatel1204@gmail.com>
2025-08-28 10:17:24 +0530

    change url

--- config/app.php
+++ config/app.php
@@ -55,8 +55,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://siddhivinayakpump.com/'),
-    'asset_url' => env('ASSET_URL', 'https://siddhivinayakpump.com/public'),
+    'url' => env('APP_URL', 'http://127.0.0.1:8000/'),
+    'asset_url' => env('ASSET_URL', 'http://127.0.0.1:8000/public'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
