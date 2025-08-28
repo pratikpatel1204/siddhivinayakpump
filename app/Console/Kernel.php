c0693c0ce32f8d16ce2543a6c537f4f8e1cbc2b9
pratikpatel1204 <www.pratikpatel1204@gmail.com>
2025-08-28 10:28:57 +0530

    change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,4 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('rewardspoint:expired')->everyMinute();
-    }
-
-    protected $commands = [
-        \App\Console\Commands\rewardspointexpired::class,
-    ];
 }
--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://127.0.0.1:8000/'),
-    'asset_url' => env('ASSET_URL', 'http://127.0.0.1:8000/public'),
+    'asset_url' => env('ASSET_URL', 'http://127.0.0.1:8000/'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
